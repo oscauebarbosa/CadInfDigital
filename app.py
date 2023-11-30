87a9246e840f525b608fc60bb185639c5d6b6331
oscauebarbosa <124931002+oscauebarbosa@users.noreply.github.com>
2023-11-30 11:17:07 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -32,6 +32,31 @@ def criar():
     lista.append(obj)
     return redirect('/influenciadores')
 
+@app.route('/excluir/<nomeinf>', methods=['GET','DELETE'])
+def excluir(nomeinf) :
+    for i, inf in enumerate(lista):
+        if inf.nome == nomeinf:
+            lista.pop(i)
+            break
+    return redirect('/influenciadores')
+
+@app.route('/editar/<nomeinf>', methods=['GET'])
+def editar(nomeinf):
+    for i, inf in enumerate(lista):
+        if inf.nome == nomeinf:
+            return render_template("Editar.html", influenciadores=inf, Titulo="Alterar Influenciador")
+
+@app.route('/alterar', methods = ['POST','PUT'])
+def alterar():
+    nome = request.form['nome']
+    for i, inf in enumerate(lista):
+        if inf.nome == nome:
+            inf.nome = request.form['nome']
+            inf.plataforma = request.form['plataforma']
+            inf.seguidores = request.form['seguidores']
+            inf.interesse = request.form['interesse']
+    return redirect('/influenciadores')
+
 
 
 if __name__ == '__main__':
